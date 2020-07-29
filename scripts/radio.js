13dac95d547e42400d9e69fddceec08fae11ac26
Stepan0305 <66253011+Stepan0305@users.noreply.github.com>
2020-07-29 14:19:35 +0600

    111

--- scripts/radio.js
+++ scripts/radio.js
@@ -1,3 +1,56 @@
 export const radioInit = () => {
-    console.log('radio connected')
+    const radio = document.querySelector('.radio');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioHeader = document.querySelector('.radio-header__big');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioStop = document.querySelector('.radio-stop');
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radioStop.disabled = true;
+
+    const selectItem = parent => {
+        radioItem.forEach(element => {
+            element.classList.remove('select');
+        });
+        parent.classList.add('select');
+    }
+
+
+    const toggleIcon = () => {
+        if (audio.paused) {
+            radio.classList.remove('play');
+            radioStop.classList.replace('fa-pause', 'fa-play');
+        } else {
+            radio.classList.add('play');
+            radioStop.classList.replace('fa-play', 'fa-pause');
+        }
+    }
+
+    radioNavigation.addEventListener('change', event => {
+        const target = event.target;
+        const parent = target.closest('.radio-item');
+        const title = parent.querySelector('.radio-name');
+        const img = parent.querySelector('.radio-img').src;
+
+        radioHeader.textContent = title.textContent;
+        radioCoverImg.src = img;
+        selectItem(parent)
+        radioStop.disabled = false;
+        audio.src = target.dataset.radioStation;
+        audio.play();
+        toggleIcon();
+    })
+
+    radioStop.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+            toggleIcon();
+        } else {
+            audio.pause();
+            toggleIcon();
+        }
+    })
 }
